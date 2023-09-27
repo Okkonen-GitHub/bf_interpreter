ce3e986ff65cd33ebed9762ae99d71b3d051629e
Okko <okko.contact@proton.me>
2023-09-27 19:03:59 +0300

    autofmt

--- src/main.rs
+++ src/main.rs
@@ -4,29 +4,33 @@ const TAPE_LEN: usize = 30_000;
 
 #[derive(PartialEq, Debug)]
 enum Command {
-    MvRight,    // >
-    MvLeft,     // <
-    Increment,  // +
-    Decrement,  // -
-    Output,     // .
-    Input,      // ,
-    JumpPast,   // [
-    JumpBack,   // ]
+    MvRight,   // >
+    MvLeft,    // <
+    Increment, // +
+    Decrement, // -
+    Output,    // .
+    Input,     // ,
+    JumpPast,  // [
+    JumpBack,  // ]
 }
 
 impl std::fmt::Display for Command {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         use Command as C;
-        write!(f, "{}", match self {
-            C::MvRight    => '>',
-            C::MvLeft     => '<',
-            C::Increment  => '+',
-            C::Decrement  => '-',
-            C::Output     => '.',
-            C::Input      => ',',
-            C::JumpPast   => '[',
-            C::JumpBack   => ']',
-        })
+        write!(
+            f,
+            "{}",
+            match self {
+                C::MvRight => '>',
+                C::MvLeft => '<',
+                C::Increment => '+',
+                C::Decrement => '-',
+                C::Output => '.',
+                C::Input => ',',
+                C::JumpPast => '[',
+                C::JumpBack => ']',
+            }
+        )
     }
 }
 
@@ -34,7 +38,7 @@ fn lex(source: &str) -> Vec<Command> {
     use Command as C;
 
     let mut commands = vec![];
-    
+
     for ch in source.chars() {
         let command = match ch {
             '>' => Some(C::MvRight),
@@ -45,8 +49,7 @@ fn lex(source: &str) -> Vec<Command> {
             ',' => Some(C::Input),
             '[' => Some(C::JumpPast),
             ']' => Some(C::JumpBack),
-            _ => None
-
+            _ => None,
         };
         if let Some(command) = command {
             commands.push(command);
@@ -55,7 +58,6 @@ fn lex(source: &str) -> Vec<Command> {
 
     return commands;
 }
-
 
 fn run(commands: Vec<Command>, ascii_mode: bool) {
     use Command as C;
@@ -72,19 +74,21 @@ fn run(commands: Vec<Command>, ascii_mode: bool) {
 
         match command {
             C::MvRight => {
-                if idx == TAPE_LEN-1 { idx = 0 }
-                else { idx += 1 }
-            },
+                if idx == TAPE_LEN - 1 {
+                    idx = 0
+                } else {
+                    idx += 1
+                }
+            }
             C::MvLeft => {
-                if idx == 0 { idx = TAPE_LEN-1 }
-                else { idx -= 1 }
-            },
-            C::Increment => {
-                state[idx] += 1
-            },
-            C::Decrement => {
-                state[idx] -= 1
-            },
+                if idx == 0 {
+                    idx = TAPE_LEN - 1
+                } else {
+                    idx -= 1
+                }
+            }
+            C::Increment => state[idx] += 1,
+            C::Decrement => state[idx] -= 1,
             C::Output => {
                 let it = state[idx].0;
                 if ascii_mode {
@@ -110,10 +114,9 @@ fn run(commands: Vec<Command>, ascii_mode: bool) {
                         } else if commands[cdx] == C::JumpBack {
                             jc -= 1
                         }
-
                     }
                 }
-            },
+            }
             C::JumpBack => {
                 if state[idx] != std::num::Wrapping(0) {
                     let mut jc: usize = 1;
@@ -127,23 +130,22 @@ fn run(commands: Vec<Command>, ascii_mode: bool) {
                     }
                     cdx -= 1
                 }
-            },
+            }
         }
         cdx += 1;
     }
 }
 
-
 fn main() {
     let args = std::env::args().collect::<Vec<String>>();
-    
+
     if args.len() <= 1 {
         println!("Usage:\nbf_interpreter <program.b[f]>");
         return;
     }
     let ascii_mode = match args.iter().nth(1) {
-        Some(arg) => {arg == "-a" || arg == "--ascii"},
-        None => false
+        Some(arg) => arg == "-a" || arg == "--ascii",
+        None => false,
     };
     let path = match args.last() {
         Some(path) => {
@@ -153,17 +155,17 @@ fn main() {
                 eprintln!("Warning: Possibly not a brainf*ck sourcefile");
                 path
             }
-        },
-        None => panic!("No second argument passed")
+        }
+        None => panic!("No second argument passed"),
     };
-    
+
     let program_file = std::fs::read_to_string(path);
     let source = match program_file {
         Ok(file) => file,
         Err(e) => {
-            eprintln!("Error reading source file: {}", e); 
+            eprintln!("Error reading source file: {}", e);
             return;
-        },
+        }
     };
     let program = lex(&source);
     run(program, ascii_mode);
